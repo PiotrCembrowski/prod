0b7d52a6c3570b73e44cb0649b66f476c7f6afb9
PiotrCembrowski <losttape@gmail.com>
2025-12-18 19:38:00 +0100

    complete Task function added

---
+++ lib/queries.ts
@@ -0,0 +1,32 @@
+"use server";
+import db from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export async function createTask(data: { name: string; sql: string }) {}
+
+export async function completeTask(taskId: number, userId: number) {
+  // Mark task as completed
+  db.prepare("UPDATE tasks SET completed = 1 WHERE id = ?").run(taskId);
+
+  // Award XP
+  db.prepare(
+    `
+    UPDATE users
+    SET experience = experience + 10
+    WHERE id = ?
+  `
+  ).run(userId);
+
+  // 3. Check for Level Up (Example: Level up every 100 EXP)
+  const user = db
+    .query("SELECT experience, level FROM users WHERE id = ?")
+    .get(userId) as { experience: number; level: number };
+
+  if (user.experience >= user.level * 100) {
+    db.prepare(
+      "UPDATE users SET level = level + 1, health = health + 2 WHERE id = ?"
+    ).run(userId);
+  }
+
+  revalidatePath("/dashboard");
+}
