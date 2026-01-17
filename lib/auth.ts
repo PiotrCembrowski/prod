091cdc23fa6697935de7e966b57ccde206d7b9e4
PiotrCembrowski <losttape@gmail.com>
2026-01-17 14:46:59 +0100

    auth fixed

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,21 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-
 import { db } from "@/lib/db";
-import { schema } from "@/lib/schema";
+import * as schema from "@/lib/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     schema,
     provider: "sqlite",
   }),
+
+  cookies: {
+    session: {
+      name: "better-auth.session",
+    },
+  },
+
+  emailAndPassword: {
+    enabled: true,
+  },
 });
